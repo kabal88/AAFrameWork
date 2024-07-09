cce63894d55665a4c6d4109a23dc38a5fcef1a58
Alexander K. <askabalkin@gmail.com>
2024-07-10 00:44:30 +0300

    add debug

---
+++ Unity/Debug/DebugDispatcher.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace AAFrameWork.Unity.Debug
+{
+    public static class DebugDispatcher
+    {
+        public static void Log(string info)
+        {
+            UnityEngine.Debug.Log($"[{Time.frameCount}] {info}");
+        }
+
+        public static void LogError(string info)
+        {
+            UnityEngine.Debug.LogError($"[{Time.frameCount}] {info}");
+        }
+
+        public static void LogWarning(string info)
+        {
+            UnityEngine.Debug.LogWarning($"[{Time.frameCount}] {info}");
+        }
+    }
+}
