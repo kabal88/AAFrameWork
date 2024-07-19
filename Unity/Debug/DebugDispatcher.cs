ecc5c8262c4ccff9ce7dd168069a8dd3d82f8cf8
Alexander K. <askabalkin@gmail.com>
2024-07-19 20:04:24 +0300

    upd: debug dispetcher

--- Unity/Debug/DebugDispatcher.cs
+++ Unity/Debug/DebugDispatcher.cs
@@ -18,5 +18,20 @@ namespace AAFrameWork.Unity.Debug
         {
             UnityEngine.Debug.LogWarning($"[{Time.frameCount}] {info}");
         }
+
+        public static void Log(string info, object sender)
+        {
+            Log($"{sender}: {info}");
+        }
+        
+        public static void LogError(string info, object sender)
+        {
+            LogError($"{sender}: {info}");
+        }
+        
+        public static void LogWarning(string info, object sender)
+        {
+            LogWarning($"{sender}: {info}");
+        }
     }
 }
