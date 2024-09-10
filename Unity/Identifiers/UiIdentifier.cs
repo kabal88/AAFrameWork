c4005e1d9d056d42dcd090e9467c38d0fa40799c
Alexander K. <askabalkin@gmail.com>
2024-09-10 19:43:45 +0300

    add: ui identifier

---
+++ Unity/Identifiers/UiIdentifier.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace AAFramework.Unity.Identifiers
+{
+    [CreateAssetMenu(fileName = "UiIdentifier", menuName = "Identifiers/UiIdentifier")]
+    public class UiIdentifier : IdentifierContainer
+    {
+    }
+}
