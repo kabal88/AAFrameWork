9137e9364bb05d62156460ff3e09d9f1a0982fd7
Alexander K. <askabalkin@gmail.com>
2024-09-27 00:29:13 +0300

    add: VisualIdentifier

---
+++ Unity/Identifiers/VisualTypeIdentifier.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace AAFramework.Unity.Identifiers
+{
+    [CreateAssetMenu(fileName = "VisualTypeIdentifier", menuName = "Identifiers/VisualType")]
+    public class VisualTypeIdentifier : IdentifierContainer
+    {
+    }
+}
