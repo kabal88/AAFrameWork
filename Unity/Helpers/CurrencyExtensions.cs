4da483674200d3677bec784a15510f53ecb63ddf
Alexander K. <askabalkin@gmail.com>
2024-09-27 00:28:55 +0300

    Upd: extentions

---
+++ Unity/Helpers/CurrencyExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Helpers
+{
+    public static class CurrencyExtensions
+    {
+        public static int ConvertToCents(this decimal price)
+        {
+            return (int)(price * 100);
+        }
+        
+        public static int ConvertToCents(this int price)
+        {
+            return price * 100;
+        }
+        
+        public static double ConvertFromCents(this int price)
+        {
+            return (double)price / 100;
+        }
+    }
+}
