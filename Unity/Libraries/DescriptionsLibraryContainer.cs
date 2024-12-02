7755cadbd8e9832adebfdfa2a9fe1a64cb675fd3
Alexander K. <askabalkin@gmail.com>
2024-12-02 03:11:51 +0300

    fix: DescriptionsLibraryContainer

---
+++ Unity/Libraries/DescriptionsLibraryContainer.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Libraries
+{
+    [CreateAssetMenu(fileName = "DescriptionsLibraryContainer", menuName = "Descriptions/Descriptions Library")]
+    public class DescriptionsLibraryContainer : ScriptableObject
+    {
+        [SerializeField] private DescriptionsLibrary descriptionsLibrary;
+
+        public DescriptionsLibrary DescriptionsLibrary => descriptionsLibrary;
+    }
+}
