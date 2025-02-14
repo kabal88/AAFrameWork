6121f5a94a364841c1252d129eba323791308b8c
Alexander K. <askabalkin@gmail.com>
2025-02-14 11:48:11 +0300

    fix: debug frames

--- Unity/Debug/DebugDispatcher.cs
+++ Unity/Debug/DebugDispatcher.cs
@@ -6,29 +6,46 @@ namespace AAFrameWork.Unity.Debug
     {
         public static void Log(string info)
         {
-            UnityEngine.Debug.Log($"[{Time.frameCount}] {info}");
+
+#if UNITY_EDITOR
+            var message = $"[{Time.frameCount}] {info}";
+#else
+            var message = $"{info}";
+#endif
+
+            UnityEngine.Debug.Log(message);
         }
 
         public static void LogError(string info)
         {
-            UnityEngine.Debug.LogError($"[{Time.frameCount}] {info}");
+#if UNITY_EDITOR
+            var message = $"[{Time.frameCount}] {info}";
+#else
+            var message = $"{info}";
+#endif
+            UnityEngine.Debug.LogError(message);
         }
 
         public static void LogWarning(string info)
         {
-            UnityEngine.Debug.LogWarning($"[{Time.frameCount}] {info}");
+#if UNITY_EDITOR
+            var message = $"[{Time.frameCount}] {info}";
+#else
+            var message = $"{info}";
+#endif
+            UnityEngine.Debug.LogWarning(message);
         }
 
         public static void Log(string info, object sender)
         {
             Log($"{sender}: {info}");
         }
-        
+
         public static void LogError(string info, object sender)
         {
             LogError($"{sender}: {info}");
         }
-        
+
         public static void LogWarning(string info, object sender)
         {
             LogWarning($"{sender}: {info}");
